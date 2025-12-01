7161d0eccba5c40a9a8352d5f4246ebaf9eccb70
evycham <ygruzevich8484@gmail.com>
2025-12-01 14:16:47 +0100

    weiter

--- src/main/java/hausaufgabe/aufgabe4/Teammate.java
+++ src/main/java/hausaufgabe/aufgabe4/Teammate.java
@@ -1,9 +1,20 @@
 package hausaufgabe.aufgabe4;
 
+import java.util.ArrayList;
+
 public class Teammate {
-	private String Nachname;
-	private String Vorname;
-	private String Alias;
+	private String Nachname = null;
+	private String Vorname = null;
+	private String ZweiterVorname = null;
+	private String Alias = null;
+
+	public ArrayList<String> parametersNames(){
+		ArrayList<String> res = new ArrayList<>();
+		res.add("nachname");
+		res.add("vorname");
+		res.add("alias");
+		return res;
+	}
 
 	public void setNachname(String nachname) {
 		this.Nachname = nachname;
@@ -16,8 +27,17 @@ public class Teammate {
 	public void setVorname(String vorname) {
 		this.Vorname = vorname;
 	}
+
 	public String getVorname() {
 		return this.Vorname;
+	}
+
+	public void setZweiterVorname(String _zweiterVorname) {
+		this.ZweiterVorname = _zweiterVorname;
+	}
+
+	public String getZweiterVorname() {
+		return this.ZweiterVorname;
 	}
 
 	public void setAlias(String alias) {
--- src/main/java/hausaufgabe/aufgabe4/XMLAusgabe.java
+++ src/main/java/hausaufgabe/aufgabe4/XMLAusgabe.java
@@ -1,46 +1,88 @@
 package hausaufgabe.aufgabe4;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class XMLAusgabe {
 
-	public static void Main(String[] args) throws Exception {
+	public String extractElement(String _input, String _tagName) throws Exception {
+		if(_input == null || _tagName == null || _input.trim().isBlank() || _tagName.trim().isBlank()){
+			throw new Exception("Falsche Eingabe");
+		}
 
+		int startIdx = _input.trim().indexOf("<" + _tagName + ">");
+		int endIdx = _input.trim().indexOf("</" + _tagName + ">");
 
-		List<Teammate> team = new ArrayList<>();
+		if(startIdx == -1 || endIdx == -1) {
+			throw new Exception("Falsche Eingabe!");
+		}
+		startIdx += 2 + _tagName.length();
 
-		String eingabe = "<team>\n" + "<person><vorname>Peter</vorname><nachname>Quill</nachname><alias>Starlord</al\n" + "ias></person>\n" + "<person><vorname>Rocket</vorname><nachname>Racoon</nachname><alias>---\n" + "</alias></person></team>";
+		return _input.substring(startIdx, endIdx).trim();
+	}
 
+	public List<String> extractElements(String _input, String _tagName) throws Exception {
 
-		String input = eingabe.trim();
+		int Pointer = 0;
+		ArrayList<String> allElements = new ArrayList<>();
 
-		//		// Vorprüfung
-		//		int openTest = 0;
-		//		int closeTest = 0;
-		//		int closeTest2 = 0;
-		//
-		//		for(int i = 0; i < input.length(); i++) {
-		//		 if(eingabe.trim().charAt(i) == '<') {
-		//			 openTest++;
-		//		 }
-		//		 else if(input.charAt(i) == '>') {
-		//			 closeTest++;
-		//		 }
-		//		 else if(input.charAt(i) == '/') {
-		//			 closeTest2++;
-		//		 }
-		//		}
-		//
-		//		if(openTest != closeTest || closeTest2 != closeTest) {
-		//			throw new Exception("Ein Tag wurde nicht geschlossen oder Flasche eingabe!");
-		//		}
-		//
+		while(true){
+			int startIdx = _input.trim().indexOf("<" + _tagName + ">", Pointer);
+			if(startIdx == -1){
+				System.err.println("Falsche Eingabe vom Tag (existiert nicht)!");
+				break;
+			}
 
-		String objects = "team";
-		String object = "person";
+			int endIdx = _input.trim().indexOf("</" + _tagName + ">", startIdx);
 
-		int index = input.indexOf("<" + objects + ">");
+			if(endIdx == -1){
+				throw new Exception("Falsche Eingabe! (keinen End-Tag gefunden)");
+			}
 
+			endIdx += 3 + _tagName.length();
+
+			String element = _input.substring(startIdx, endIdx).trim();
+			allElements.add(extractElement(element, _tagName));
+			Pointer = endIdx + _tagName.length() + 3;
+		}
+
+		if(allElements.isEmpty()){
+			throw new Exception("Keine Eingabe!");
+		}
+		return allElements;
+	}
+
+	public static void main(String[] args) throws Exception {
+		try {
+			StringBuilder sb = new StringBuilder();
+
+			try(BufferedReader br = new BufferedReader(new FileReader("/home/evycham/Studium/3. Semester/Software Lab/labor4/src/main/java/hausaufgabe/aufgabe4/test.xml"))) {
+				String line;
+				while((line = br.readLine()) != null) {
+					sb.append(line.trim());
+				}
+			}
+
+			String xml = sb.toString();
+
+			XMLAusgabe xmlAusgabe = new XMLAusgabe();
+
+			String teamInput = xmlAusgabe.extractElement(xml, "team");
+
+			List<String> personsInput = xmlAusgabe.extractElements(teamInput, "person");
+
+			for(String p : personsInput) {
+				List<String> vornamen = xmlAusgabe.extractElements(p, "vorname");
+				String nachname = xmlAusgabe.extractElement(p, "nachname");
+				String alias = xmlAusgabe.extractElement(p, "alias");
+				if(vornamen.size() > 1 && vornamen.get(1) == null) {
+					vornamen.add(1, "-");
+				}
+				System.out.println(vornamen.toString() + " " + nachname + " " + alias);
+			}
+		} catch(Exception ex) {
+			System.err.println(ex.getMessage());
+		}
 	}
 }
